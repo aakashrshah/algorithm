873925c23097333a432947955ec2ace948e71ecd
aakashrshah <aadi1428@gmail.com>
2016-10-18 03:33:03 -0400

    Merge Sort w/ Divide and Conquer

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -6,6 +6,7 @@ public class MergeSort {
 
 	final static int SIZE = 10;
 	static int[] outputArray = new int[SIZE];
+	
 	public static void main(String args[]){
 		int[] arraylist;
 		ArrayInput arr = new ArrayInput();
@@ -16,69 +17,58 @@ public class MergeSort {
 		}
 		System.out.println();
 		
-		
+
 		int startIndex = 0;
 		int lastIndex = arraylist.length - 1;
-		MergeSortAlgorithm(arraylist,startIndex,lastIndex);	
+		MergeSortAlgorithm(arraylist,startIndex,lastIndex,outputArray);	
+	}
+
+	private static void MergeSortAlgorithm(int[] array, int firstIndex, int lastIndex,int[] outputArray) {
+		int[] out = new int[SIZE];
 		
+		if(firstIndex == lastIndex){
+			outputArray[firstIndex] = array[firstIndex];
+			return;
+		}
+		
+		int pivot = (firstIndex + lastIndex)/2;
+		MergeSortAlgorithm(array, firstIndex, pivot,out);
+		MergeSortAlgorithm(array, pivot+1, lastIndex,out);
+		out = Merge(out,firstIndex,lastIndex,outputArray);
 		for (int i : outputArray) {
 			System.out.print(i + " ");
 		}
 		System.out.println();
 	}
 
-	private static void MergeSortAlgorithm(int[] array, int firstIndex, int lastIndex) {
+
+	private static int[] Merge(int[] array, int firstIndex, int lastIndex,int[] outputArray) {
 		int pivot = (firstIndex + lastIndex)/2;
-		if(firstIndex == lastIndex){
-			outputArray[firstIndex] = array[firstIndex];
-			return;
+		int u = firstIndex;
+		int v = pivot + 1;
+		int w = u;
+		
+		while(u <= pivot && v <= lastIndex){
+			if(array[u] <= array[v]){
+				outputArray[w] = array[u];
+				u++;
+			}
+			else{
+				outputArray[w] = array[v];
+				v++;
+			}
+			w++;
 		}
 		
-		MergeSortAlgorithm(array, firstIndex, pivot);
-		MergeSortAlgorithm(array, pivot+1, lastIndex);
-		Merge(array,firstIndex,lastIndex);
-	}
+		if(u>pivot){
+//			outputArray[w:lastIndex] = array[v:lastIndex];
+			System.arraycopy(array, v, outputArray, w, lastIndex - v + 1);
+		}else if (v > lastIndex){
+//			outputArray[w:lastIndex] = array[u:pivot];
+			System.arraycopy(array, u, outputArray, w, pivot - u + 1);
 
-
-	private static void Merge(int[] array, int firstIndex, int lastIndex) {
-		int pivot = (firstIndex + lastIndex)/2;
-	   	int u,v,w;  
-	   	/* 	u will scan C[i:k],  
-			v will scan C[k+1:j], and 
-			w will index the out B
-		*/
-	   	u=firstIndex;
-	   	v=pivot+1;
-	   	w=firstIndex;
-	   	
-	   	while  (u <= pivot && v <= lastIndex){
-	    		if (array[u] <= array[v]){
-	     			outputArray[w]=array[u]; 
-	     			u++;w++;
-	    		}
-	    		else{
-	     			outputArray[w]=array[v]; 
-	     			v++;w++;
-	    		}		
-	   	}
-	   	if(u > pivot){
-	   		while (v < lastIndex)
-	   	    {
-	   	        outputArray[w] = array[v];
-	   	        w++;
-	   	        v++;
-	   	    }
-	   	}
-	    		
-	   	else if(v>lastIndex){
-	   		while (u < pivot)
-	   	    {
-	   	        outputArray[w] = array[u];
-	   	        w++;
-	   	        u++;
-	   	    }
-	   	}
-		
+		}
+		return outputArray;
 	}
 	
 }
